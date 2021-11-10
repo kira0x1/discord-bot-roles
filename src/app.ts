914ff93de3d5da8697b5d9de6298d192c7ce3d9c
kira0x1 <kira.eva@protonmail.com>
2021-11-10 22:13:14 +0300

    Log when bot starts

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,17 @@ import { Client } from 'discord.js';
 import { syncRoles } from './util/syncRoles';
 import { initGreeter } from './util/serverGreeter';
 import { initVoiceManager, VoiceRoleManager } from './util/voiceRoleManager';
+import { logger } from './util/logger';
 
 const client = new Client({
    intents: ['GUILDS', 'GUILD_EMOJIS_AND_STICKERS', 'GUILD_MESSAGE_REACTIONS']
 });
 
-client.once('ready', () => {
-   syncRoles(client);
+client.once('ready', async () => {
    initGreeter(client);
-   initVoiceManager(client);
+   await syncRoles(client);
+   await initVoiceManager(client);
+   logger.info(`mikaela-roles online`);
 });
 
 client.login(token);
