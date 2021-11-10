8045d13fa1c1d9ded2623bdcde761bfc4db8744f
kira0x1 <kira.eva@protonmail.com>
2021-11-10 21:59:38 +0300

    Add voice manager

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { token } from './config';
 import { Client } from 'discord.js';
 import { syncRoles } from './util/syncRoles';
 import { initGreeter } from './util/serverGreeter';
+import { initVoiceManager, VoiceRoleManager } from './util/voiceRoleManager';
 
 const client = new Client({
    intents: ['GUILDS', 'GUILD_EMOJIS_AND_STICKERS', 'GUILD_MESSAGE_REACTIONS']
@@ -10,6 +11,7 @@ const client = new Client({
 client.once('ready', () => {
    syncRoles(client);
    initGreeter(client);
+   initVoiceManager(client);
 });
 
 client.login(token);
